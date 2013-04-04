b721e13415643dfe1085d94fd45b53c5ada8f90b
Kenneth Baltrinic <kenneth@baltrinic.com>
2013-04-03 22:59:04 -0400

    Updates selenium install script to install correct chromedriver based on OS.

--- selenium/server/install.sh
+++ selenium/server/install.sh
@@ -1,5 +1,18 @@
 #! /bin/bash
+ENV=$(uname -s)
+case "$ENV" in
+Darwin)
+  CHROME_DRIVER="chromedriver_mac_26.0.1383.0.zip"
+  ;;
+Linux)
+  CHROME_DRIVER="chromedriver_linux64_26.0.1383.0.zip"
+  ;;
+*)
+  echo "Uknown environment: $ENV"
+  exit 1
+  ;;
+esac
 wget http://selenium.googlecode.com/files/selenium-server-standalone-2.31.0.jar
-wget https://chromedriver.googlecode.com/files/chromedriver_mac_26.0.1383.0.zip
-unzip chromedriver_mac_26.0.1383.0.zip
-rm -f chromedriver_mac_26.0.1383.0.zip
+wget https://chromedriver.googlecode.com/files/$CHROME_DRIVER
+unzip $CHROME_DRIVER
+rm -f $CHROME_DRIVER
